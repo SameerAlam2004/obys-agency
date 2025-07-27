a5ddb36c43bbdf06f688ac796c11f51846eabcac
SameerAlam2004 <sam1923027@gmail.com>
2025-07-28 01:57:28 +1245

    page1 done

--- script.js
+++ script.js
@@ -1,47 +1,88 @@
+function loadingAnimation() {
 
-var tl = gsap.timeline()
+  var tl = gsap.timeline()
 
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.1
-})
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.1
+  })
 
-tl.from("#line1part1 , .line h2", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector("#line1part1 h5")
-    var grow = 0
-    setInterval(function () {
+  tl.from("#line1part1 , .line h2", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1part1 h5")
+      var grow = 0
+      setInterval(function () {
 
-      if (grow < 100) {
+        if (grow < 100) {
 
-        h5timer.innerHTML = grow++
-        grow++
-      }
-      else {
-        h5timer.innerHTML = grow
-      }
-    }, 55)
-  }
-})
+          h5timer.innerHTML = grow++
+          grow++
+        }
+        else {
+          h5timer.innerHTML = grow
+        }
+      }, 55)
+    }
+  })
 
-tl.to("#loader", {
-  opacity: 0,
-  duration: 1,
-  delay: 2,
-})
-
-tl.from("#page1",{
-  y:1200,
-  opacity:0
-})
-
-tl.to("#loader",{
-  display:none
-})
-
-tl.to(".line h2",{
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+    onComplete: function () {
+      document.querySelector("#loader").style.display = "none";
+    }
+  })
   
-  animationName:"anime",
-  opacity:1
+
+
+  tl.from("#page1", {
+    y: 1200,
+    opacity: 0
+  })
+
+  // tl.to("#loader",{
+  //   display:none
+  // })
+
+  tl.to(".line h2", {
+
+    animationName: "anime",
+    opacity: 1
+  })
+
+  
+tl.from(".hero h1",{
+  y:100,
+  stagger:.1,
+
 })
+
+tl.from("#nav",{
+  opacity:0,
+  
+})
+
+}
+
+loadingAnimation()
+
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      x: dets.x,
+      y: dets.y,
+
+    })
+
+
+  })
+
+  Shery.makeMagnet("#navpart2 h4")
+
+
+}
+
+cursorAnimation()
